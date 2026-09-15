49646b9c829768f27980e5fb6170453d05c9381b
AlejandroWare <alejandro.lopez@colimasoft.com>
2026-09-15 12:23:26 -0600

    feat: implement task item component and integrate task management features

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Task } from './task';
+import { TaskItem } from './task-item/task-item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskItem],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('task-manager');
-  protected readonly tasks = [
+  protected readonly tasks: Task[] = [
     { id: 1, title: 'Aprender signals', done: false, priority: 'high' },
     { id: 2, title: 'Conectar InsForge', done: false, priority: 'medium' },
     { id: 3, title: 'Armar el login', done: true, priority: 'low' },
   ];
+
+  onToggle(id: number) {
+
+  }
+
+  onRemove(id: number) {
+
+  }
 }
